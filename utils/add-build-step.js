9dba7ce6387b2eccee276cd16e587d35890bbca5
Alec Reynolds <alec+git@lando.dev>
2023-12-06 21:55:20 -0800

    Refactor utils/correct path.

--- utils/add-build-step.js
+++ utils/add-build-step.js
@@ -7,17 +7,8 @@ const _ = require('lodash');
  * Helper to get global deps
  * @TODO: this looks pretty testable? should services have libs?
  */
-exports.addBuildStep = (steps, app, name, step = 'build_internal', front = false) => {
+module.exports = (steps, app, name, step = 'build_internal', front = false) => {
   const current = _.get(app, `config.services.${name}.${step}`, []);
   const add = (front) ? _.flatten([steps, current]) : _.flatten([current, steps]);
   _.set(app, `config.services.${name}.${step}`, _.uniq(add));
 };
-
-/*
- * Helper to get global deps
- * @TODO: this looks pretty testable? should services have libs?
- */
-exports.getInstallCommands = (deps, pkger, prefix = []) => _(deps)
-  .map((version, pkg) => _.flatten([prefix, pkger(pkg, version)]))
-  .map(command => command.join(' '))
-  .value();
